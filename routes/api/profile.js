d1ac3f730e7abe5fc1a745c5a39c4811d2b45766
Nobu <rockmusic2090@gmail.com>
2020-02-27 15:42:05 -0800

    Added private path to get profle data

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const auth = require("../../middleware/auth");
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 const router = express.Router();
 
-// @route   api/profile
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => {
-  res.send("Profile route");
+// @route   api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await (
+      await Profile.findOne({ user: req.user.id })
+    ).populated("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
