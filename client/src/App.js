e36a4010f6b38281ef8402f66e5810dfe96c5d7b
Nobu <rockmusic2090@gmail.com>
2020-03-01 12:03:36 -0800

    Created client folder and added Navbar component and Landing component

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
+}
+
+export default App;
