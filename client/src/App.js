87dadacbff24f7acbd0ce4eda2cd173f21f146f3
Nobu <rockmusic2090@gmail.com>
2020-03-01 12:06:55 -0800

    Added React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 
@@ -6,10 +7,12 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Landing />
-    </>
+    <Router>
+      <>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+      </>
+    </Router>
   );
 }
 
