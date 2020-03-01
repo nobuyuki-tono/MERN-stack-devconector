31d151fcda49f954a38271fdec3281b4ab7f928d
Nobu <rockmusic2090@gmail.com>
2020-03-01 12:15:40 -0800

    Created route for login and register in client

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 import "./App.css";
 
@@ -11,6 +13,12 @@ function App() {
       <>
         <Navbar />
         <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
       </>
     </Router>
   );
